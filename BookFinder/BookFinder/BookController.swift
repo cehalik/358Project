977142f139ced5d5cd0f86c90ae4deaca193178f
cehalik <60154052+cehalik@users.noreply.github.com>
2020-04-27 10:06:17 -0500

    Update

---
+++ BookFinder/BookFinder/BookController.swift
@@ -0,0 +1,30 @@
+//
+//  BookController.swift
+//  BookFinder
+//
+//  Created by Beverly L Brown on 4/27/20.
+//  Copyright © 2020 Chris Halikias. All rights reserved.
+//
+
+import UIKit
+
+class BookController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookFinder/BookFinder/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BookFinder
+//
+//  Created by Beverly L Brown on 4/2/20.
+//  Copyright © 2020 Chris Halikias. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
